53f86266443fcfd8ea9c6cceb43c5aae21dd1f15
Marcos <marcosreinoso2012@gmail.com>
2020-07-14 18:43:55 -0300

    Ejercicio 6

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	numeroUno = txtIdNumeroUno.value;
+	numeroDos = txtIdNumeroUno.value;
+	numeroUno= parseInt (numeroUno);
+	numeroDos= parseInt (numeroDos);
+	suma = numeroUno + numeroDos;
+	alert(suma);
+
+
+	
 }
 
